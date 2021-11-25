4ab4f293f45eaaceea76a6f1ac46ef9ae6747187
Katherine Cornejo <86565087+Kathecot@users.noreply.github.com>
2021-11-25 14:09:03 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 function compute() {
-    principal = document.getElementById("principal").value;
-    rate = document.getElementById("rate").value;
-    years = document.getElementById("years").value;
-    interest = principal * years * rate / 100
-    actual_year = new Date().getFullYear()
-    future_year = actual_year + Number(years)
+    var amount = document.getElementById("Amount").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = amount * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var result = document.getElementById("result");
+    result.innerHTML = `If you deposit <mark>${amount}</mark>,<br>at an interest rate of <mark>${rate}%</mark>.<br>You will receive an amount of <mark>${interest}</mark>,<br>in the year <mark>${year}</mark>`;
 
-    if (validate_numer() == true) {
-        document.getElementById("result").innerHTML =
-            `If you deposit <mark>${principal}</mark>,<br />at an interest rate of <mark>${rate}</mark>.<br />You will receive an amount of <mark>${interest}</mark>,<br />in the year <mark>${future_year}</mark>`
-    }
-}
-
-function read_value() {
-    document.getElementById("rate_value").innerHTML = document.getElementById("rate").value + '%';
 
 }
 
-function validate_numer() {
-    amount = document.getElementById("principal").value;
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
 
+function warning() {
+    var amount = document.getElementById("Amount").value;
     if (amount <= 0) {
-        alert('Enter a positive number')
-        document.getElementById("principal").focus()
-        return false
+        alert("Enter a positive number");
+        document.getElementById("Amount").focus();
+
     }
 
-    return true
+
 }
